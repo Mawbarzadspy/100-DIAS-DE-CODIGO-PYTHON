7e13ee2adf893cb4d44fcdb669412a3d1f2003f0
Mawbarzadspy <mawbarzadspy@gmail.com>
2022-02-10 23:19:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 100-DIAS-DE-CODIGO-PYTHON
+ Projeto com 100 códigos em Python para te ajudar na caminhada.
---
+++ ex001.py
@@ -0,0 +1,26 @@
+print('Olá, Mundo!')
+print("Tem que ser sempre olá Mundo?")
+print('Pra quem está iniciando isso não parece ser um programa.')
+print("\nIsso aqui tem mais cara de programa. ")
+
+print('\nBelo é melhor que feio.'
+      '\nExplícito é melhor que implícito.'
+      '\nSimples é melhor que complexo.'
+      '\nComplexo é melhor que complicado.'
+      '\nPlano é melhor que aninhado.'
+      '\nEsparso é melhor que denso.'
+      '\nLegibilidade conta.'
+      '\nCasos especiais não são especiais o suficiente para violar as regras.'
+      '\nEmbora praticidade vença pureza.'
+      '\nErros não devem passar silenciosamente.'
+      '\nA não ser que sejam explicitamente silenciados.'
+      '\nEm caso de ambiguidade, resista à tentação de adivinhar.'
+      '\nDeve haver um e somente um jeito óbvio de fazer.'
+      '\nEmbora tal jeito não seja tão óbvio no à primeira vista a não ser que você seja holandês.'
+      '\nAgora é melhor que nunca.'
+      '\nEmbora nunca é frequentemente melhor que exatamente agora.'
+      '\nSe a implementação é difícil de explicar, a ideia é ruim.'
+      '\nSe a implementação é fácil de explicar, talvez a ideia seja boa.'
+      '\nEspaços de nomes são uma ideia estupenda vamos fazer mais deles!')
+
+
